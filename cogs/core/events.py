6bc4baa2dbd6d1e62700a931aff93ca1a6e87ec2
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-12 17:42:46 -0500

    Added config channels

--- cogs/core/events.py
+++ cogs/core/events.py
@@ -12,6 +12,10 @@ class Events(commands.Cog):
         """Event triggered when the bot's cache is loaded"""
 
         self.bot.me = self.bot.get_user(self.bot.c.me)
+        self.bot.guild = self.bot.get_guild(self.bot.c.guild_id)
+
+        self.bot.error_channel = self.bot.guild.get_channel(self.bot.c.error_channel_id)
+        self.bot.testing_channel - self.bot.guild.get_channel(self.bot.c.testing_channel_id)
 
         print("\nONLINE\n")
 
