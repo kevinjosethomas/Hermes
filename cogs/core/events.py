5b20f7d319c17907d7a1c880eb91fdaa10ee15bc
TrustedMercury <kevin.jt2007@gmail.com>
2020-11-11 21:01:59 -0500

    Added on_ready event

--- cogs/core/events.py
+++ cogs/core/events.py
@@ -7,6 +7,12 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """Event triggered when the bot's cache is loaded"""
+
+        print("\nONLINE\n")
+        
 
 def setup(bot):
     bot.add_cog(Events(bot))
